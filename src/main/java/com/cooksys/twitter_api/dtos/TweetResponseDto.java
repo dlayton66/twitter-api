b6a5f88d2ec123a9800ac22aa7a1b61066a73e11
Drew Layton <dlayton66@gmail.com>
2023-11-27 18:00:48 -0600

    feat: TweetResponseDto

---
+++ src/main/java/com/cooksys/twitter_api/dtos/TweetResponseDto.java
@@ -0,0 +1,21 @@
+package com.cooksys.twitter_api.dtos;
+
+import com.cooksys.twitter_api.entities.Tweet;
+import com.cooksys.twitter_api.entities.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+public class TweetResponseDto {
+
+  private Long id;
+  private User author;
+  private Timestamp posted;
+  private String content;
+  private Tweet inReplyTo;
+  private Tweet repostOf;
+
+}
