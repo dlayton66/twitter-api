f034d40694974db1cf9d51a4b9f8034f7a363146
Drew Layton <dlayton66@gmail.com>
2023-11-28 10:23:46 -0600

    feat: TweetRequestDtoDto

---
+++ src/main/java/com/cooksys/twitter_api/dtos/TweetRequestDto.java
@@ -0,0 +1,13 @@
+package com.cooksys.twitter_api.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class TweetRequestDto {
+
+  private String content;
+  private CredentialsDto credentials;
+
+}
