1852e94cab32f5725c3ac44e8a579cf2f438ff4c
Padraic Reilly <patrick.reilly22@gmail.com>
2023-11-28 12:16:34 -0800

    Added all "/tweets" routes exist in TweetController class and TweetService interface.

--- src/main/java/com/cooksys/twitter_api/controllers/TweetController.java
+++ src/main/java/com/cooksys/twitter_api/controllers/TweetController.java
@@ -1,10 +1,10 @@
 package com.cooksys.twitter_api.controllers;
 
-import com.cooksys.twitter_api.dtos.TweetResponseDto;
+import com.cooksys.twitter_api.dtos.*;
+import com.cooksys.twitter_api.services.TweetService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,10 +15,67 @@ public class TweetController {
     private TweetService tweetService;
 
     @GetMapping
-    List<TweetResponseDto> getAllTweets(){
+    public List<TweetResponseDto> getAllTweets(){
         return tweetService.getAllTweets();
     }
 
+    @PostMapping
+    public TweetResponseDto createTweet(@RequestBody TweetRequestDto tweetRequestDto, @RequestBody CredentialsDto credentialsDto){
+        return tweetService.createTweet(tweetRequestDto, credentialsDto);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<TweetResponseDto> getTweetById(@PathVariable int id){
+        return tweetService.getTweetById(id);
+    }
 
+    @GetMapping("/{id}/replies")
+    public ResponseEntity<List<TweetResponseDto>> getRepliesToTweet(@PathVariable int id){
+        return tweetService.getRepliesToTweet(id);
+    }
+
+    @GetMapping("/{id}/mentions")
+    public ResponseEntity<List<UserResponseDto>> getUsersMentionedInTweet(@PathVariable int id){
+        return tweetService.getUsersMentionedInTweet(id);
+    }
+
+    @GetMapping("/{id}/reposts")
+    public ResponseEntity<List<TweetResponseDto>> getRepostsOfTweet(@PathVariable int id){
+        return tweetService.getRepostsOfTweet(id);
+    }
+
+    @PostMapping("/{id}/repost")
+    public ResponseEntity<TweetResponseDto> repostTweet(@PathVariable int id, @RequestBody CredentialsDto credentialsDto){
+        return tweetService.repostTweet(id, credentialsDto);
+    }
+
+    @GetMapping("/{id}/context")
+    public ResponseEntity<ContextDto> getContextOfTweet(@PathVariable int id){
+        return tweetService.getContextOfTweet(id);
+    }
+
+    @GetMapping("/{id}/likes")
+    public ResponseEntity<UserResponseDto> getLikesOnTweet(@PathVariable int id){
+        return tweetService.getLikesOnTweet(id);
+    }
+
+    @GetMapping("/{id}/tags")
+    public ResponseEntity<HashtagDto> getHashtagsOnTweet(@PathVariable int id){
+        return tweetService.getHashtagsOnTweet(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<TweetResponseDto> deleteTweet(@PathVariable int id, @RequestBody CredentialsDto credentialsDto){
+        return tweetService.deleteTweet(id, credentialsDto);
+    }
+
+    @PostMapping("/{id}/like")
+    public void likeTweet(@PathVariable int id, @RequestBody CredentialsDto credentialsDto){
+        tweetService.likeTweet(id, credentialsDto);
+    }
+
+    @PostMapping("/{id}/reply")
+    public void replyToTweet(@PathVariable int id, @RequestBody CredentialsDto credentialsDto, @RequestBody String content){
+        tweetService.replyToTweet(id, credentialsDto, content);
+    }
 }
