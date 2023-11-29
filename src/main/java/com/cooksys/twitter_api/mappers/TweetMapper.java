3f68fb5ddbf853600d528faf05f1c98f7434679e
Padraic Reilly <patrick.reilly22@gmail.com>
2023-11-28 17:40:47 -0800

    Create HashTagRepository, UserRepository, update all Mapper interfaces.
    Added empty interface classes.
    
    Added the annotations on
    @Mapper(componentModel = "spring") to all Mapper interfaces to cause the MapStruct generation of the MapperImpl classes.

--- src/main/java/com/cooksys/twitter_api/mappers/TweetMapper.java
+++ src/main/java/com/cooksys/twitter_api/mappers/TweetMapper.java
@@ -3,7 +3,9 @@ package com.cooksys.twitter_api.mappers;
 import com.cooksys.twitter_api.dtos.TweetRequestDto;
 import com.cooksys.twitter_api.dtos.TweetResponseDto;
 import com.cooksys.twitter_api.entities.Tweet;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface TweetMapper {
 
   Tweet requestDtoToEntity(TweetRequestDto tweetRequestDto);
---
+++ src/main/java/com/cooksys/twitter_api/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package com.cooksys.twitter_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
+}
