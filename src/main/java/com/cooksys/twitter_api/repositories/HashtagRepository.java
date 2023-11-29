d50a1861e10cdd5416dc9d846db593647d7fbb20
Padraic Reilly <patrick.reilly22@gmail.com>
2023-11-29 13:02:48 -0800

    BugFix: Hashtags have "label"s not "content"

--- src/main/java/com/cooksys/twitter_api/repositories/HashtagRepository.java
+++ src/main/java/com/cooksys/twitter_api/repositories/HashtagRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Integer> {
-    Optional<Hashtag> findByContent(String content);
+    Optional<Hashtag> findByLabel(String label);
 }
 
