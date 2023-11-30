ba07d4df87e4ed03c82898a8a193f00ff869a748
Padraic Reilly <patrick.reilly22@gmail.com>
2023-11-29 18:26:26 -0800

    Changes to HashtagMapper, User entity
    User entity: initialized Set fields to empty sets.
    
    HashtagMapper: added List->List mapping methods.

--- src/main/java/com/cooksys/twitter_api/mappers/HashtagMapper.java
+++ src/main/java/com/cooksys/twitter_api/mappers/HashtagMapper.java
@@ -4,11 +4,17 @@ import com.cooksys.twitter_api.dtos.HashtagDto;
 import com.cooksys.twitter_api.entities.Hashtag;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface HashtagMapper {
 
   Hashtag dtoToEntity(HashtagDto hashtagDto);
 
   HashtagDto entityToDto(Hashtag hashtag);
+
+  List<HashtagDto> entitiesToDto(List<Hashtag> hashtags);
+
+  List<Hashtag> DtoToEntities(List<HashtagDto> hashtagDtos);
 
 }
