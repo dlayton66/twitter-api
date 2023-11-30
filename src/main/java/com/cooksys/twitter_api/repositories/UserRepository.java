b548715317cfcf80350326ade6e6b3b29eb38c15
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-11-29 22:01:59 -0600

    Update UserRepository.java

--- src/main/java/com/cooksys/twitter_api/repositories/UserRepository.java
+++ src/main/java/com/cooksys/twitter_api/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import com.cooksys.twitter_api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByCredentialsUsername(String username);
 
